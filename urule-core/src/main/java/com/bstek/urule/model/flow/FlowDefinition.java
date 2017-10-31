cfca747b5be0118e751ba58fac78224d3bf40660
gaopeihong <gaopeihong@51talk.com>
2017-10-31 22:19:28 +0800

    修复客户端无法解析嵌套规则流节点跳转的bug

--- urule-core/src/main/java/com/bstek/urule/model/flow/FlowDefinition.java
+++ urule-core/src/main/java/com/bstek/urule/model/flow/FlowDefinition.java
@@ -83,17 +83,6 @@ public class FlowDefinition implements ProcessDefinition {
 				String nodeName=conn.getToName();
 				conn.setTo(getFlowNode(nodeName));
 			}
-			if(node instanceof RuleNode){
-				KnowledgePackageWrapper wrapper = ((RuleNode) node).getKnowledgePackageWrapper();
-				wrapper.buildDeserialize();
-				KnowledgePackage knowledgePackage=wrapper.getKnowledgePackage();
-				Map<String, FlowDefinition> flowMap=knowledgePackage.getFlowMap();
-				if(flowMap!=null && flowMap.size()>0){
-					for(FlowDefinition fd:flowMap.values()){
-						fd.buildConnectionToNode();
-					}
-				}
-			}
 		}
 	}
 	
